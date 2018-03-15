fbd9b8ecb19eeb4d21ce3615e96052d67c7281e9
beacon <78319709@qq.com>
2018-03-15 16:10:09 +0800

    断容器

---
+++ 1.5.10/cloud-hystrixclient/src/main/java/cn/com/beacon/controller/ConsumerController.java
@@ -0,0 +1,22 @@
+package cn.com.beacon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.com.beacon.remote.HelloRemote;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private HelloRemote helloremote;
+	
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return helloremote.hello(name);
+    }
+
+    
+}
