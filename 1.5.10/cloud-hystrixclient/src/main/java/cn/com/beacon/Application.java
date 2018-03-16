11169ad40ccccd9fe0d825dda4525003199eb689
beacon <78319709@qq.com>
2018-03-16 09:48:18 +0800

    提交

--- 1.5.10/cloud-hystrixclient/src/main/java/cn/com/beacon/Application.java
+++ 1.5.10/cloud-hystrixclient/src/main/java/cn/com/beacon/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableHystrix
 public class Application {
 
 	public static void main(String[] args) {
