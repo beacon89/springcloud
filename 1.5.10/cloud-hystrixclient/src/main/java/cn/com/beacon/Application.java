719b65653534002fbf3abd86c0075afcf0a5dcd0
beacon <78319709@qq.com>
2018-03-16 11:59:50 +0800

    监控问题

--- 1.5.10/cloud-hystrixclient/src/main/java/cn/com/beacon/Application.java
+++ 1.5.10/cloud-hystrixclient/src/main/java/cn/com/beacon/Application.java
@@ -5,15 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
-@EnableHystrixDashboard
-@EnableHystrix
 public class Application {
 
 	public static void main(String[] args) {
