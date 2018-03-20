dae61364e99b9e6af537713e4a73cdc87f79109c
beacon <78319709@qq.com>
2018-03-20 10:30:21 +0800

    提交

--- 1.5.2/kayak-tools/cloud-sleuthservice/src/main/java/cn/com/beacon/Application.java
+++ 1.5.2/kayak-tools/cloud-sleuthservice/src/main/java/cn/com/beacon/Application.java
@@ -3,11 +3,12 @@ package cn.com.beacon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZipkinServer
 public class Application {
 
 	public static void main(String[] args) {
--- 1.5.2/kayak-tools/cloud-toolservice/src/main/java/cn/com/beacon/controller/FindEurekaRegisteredService.java
+++ 1.5.2/kayak-tools/cloud-toolservice/src/main/java/cn/com/beacon/controller/FindEurekaRegisteredService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class FindEurekaRegisteredService {
 
